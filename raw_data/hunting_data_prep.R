ad533afc636d1f55d86c6eb6d58cc70bb2f37d49
Guillermo Garcia Costoya <ggarciacosto@gmail.com>
2021-04-27 11:44:32 -0700

    include mcelreath paper data

---
+++ raw_data/hunting_data_prep.R
@@ -0,0 +1,11 @@
+## McElreath paper data
+
+# load package here
+library(here)
+
+# load data
+hunting_mcelreath <- read.csv(file.path(here(), "raw_data", "hunting.csv"))
+
+# save paper data in "data" folder
+save(hunting_mcelreath, file = file.path(here::here(), "data", "hunting_mcelreath.RData"))
+l
